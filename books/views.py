57db83ada8ecb50ab38356bea4ab21d385e0add3
qtpython <qtkhajacloud@gmail.com>
2021-09-14 18:31:02 +0530

    Added code to insert publisher to database from django view

--- books/views.py
+++ books/views.py
@@ -14,6 +14,12 @@ def create_publisher(request):
     """
     This view will create the publisher in the database
     """
-    print(request)
-    return HttpResponse('Testing')
+    try:
+        name = request.GET.get('name')
+        email = request.GET.get('email')
+        website = request.GET.get('website')
+        Publisher.objects.create(name=name, email=email, website=website)
+    except Exception as e:
+        return HttpResponse(f"Couldnot insert record {e}")
+    return HttpResponse("Record Inserted successfully")
 
